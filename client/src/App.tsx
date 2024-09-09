d9ae1174d2bf55308729cbd5d3563af74bf101b2
lij0825 <dldlswns890@gmail.com>
2024-09-09 21:54:03 +0900

    [FEAT] firebase 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,27 @@ import Header from "components/common/Header";
 import MainRoute from "components/routes/MainRoute";
 import { Route, Routes, useLocation } from "react-router-dom";
 
+// firebase setup
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getMessaging } from "firebase/messaging";
+import { get } from "http";
+// https://firebase.google.com/docs/web/setup#available-libraries
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
+const messaging = getMessaging(firebaseApp);
+
+// firebase setup
+
 function App() {
   const location = useLocation();
   const currentPath = location.pathname;
@@ -18,9 +39,7 @@ function App() {
   const shouldHideHeaderFooter = popupPaths.includes(currentPath) && isPopup;
   return (
     <div className="pb-[86px] relative">
-      {!shouldHideHeaderFooter && showHeaderPaths.includes(currentPath) && (
-        <Header />
-      )}
+      {!shouldHideHeaderFooter && showHeaderPaths.includes(currentPath) && <Header />}
       <Routes>
         <Route path="/*" element={<MainRoute />} />
       </Routes>
