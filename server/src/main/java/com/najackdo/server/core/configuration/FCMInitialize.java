36639e0582fcc15f30cfb54e523608db88246d82
이인준 <dldlswns890@naver.com>
2024-09-10 10:54:51 +0900

    [FIX] FCM initialize 리팩토링

--- server/src/main/java/com/najackdo/server/core/configuration/FCMInitialize.java
+++ server/src/main/java/com/najackdo/server/core/configuration/FCMInitialize.java
@@ -1,6 +1,7 @@
 package com.najackdo.server.core.configuration;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
@@ -16,22 +17,33 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
 public class FCMInitialize {
+
 	@Value("${fcm.certification}")
 	private String credentials;
 
 	@PostConstruct
-	public void initialize() throws IOException {
+	public void initialize() {
 		try {
-			ClassPathResource resource = new ClassPathResource(credentials);
+			initializeFirebaseApp();
+		} catch (IOException e) {
+			log.error("IOException while initializing FirebaseApp: {}", e.getMessage());
+		} catch (Exception e) {
+			log.error("Unexpected error while initializing FirebaseApp: {}", e.getMessage());
+		}
+	}
+
+	private void initializeFirebaseApp() throws IOException {
+		ClassPathResource resource = new ClassPathResource(credentials);
+
+		try (InputStream inputStream = resource.getInputStream()) {
 			FirebaseOptions options = FirebaseOptions.builder()
-				.setCredentials(GoogleCredentials.fromStream(resource.getInputStream()))
+				.setCredentials(GoogleCredentials.fromStream(inputStream))
 				.build();
+
 			if (FirebaseApp.getApps().isEmpty()) {
 				FirebaseApp.initializeApp(options);
 				log.info("FirebaseApp initialization complete");
 			}
-		} catch (Exception e) {
-			log.error("Error while initializing FirebaseApp: {}", e.getMessage());
 		}
 	}
 }
